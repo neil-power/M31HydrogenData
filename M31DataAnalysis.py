f95ee53a2bc8d7833aee39b27f2b5583cd8095b3
neil-power <52502232+neil-power@users.noreply.github.com>
2023-03-21 19:31:31 +0000

    end of day 5

--- M31DataAnalysis.py
+++ M31DataAnalysis.py
@@ -9,7 +9,7 @@ from scipy.integrate import simpson
 MAX_DEGREE = 5
 PEAK_WIDTH = 40
 S8_FLUX = 850 #K km/s
-PD = "b-" 
+PD = "b-"
 FD = "r-"
 
 # READ IN DATA -------------------------------------------------------------
@@ -43,10 +43,13 @@ def match_coords():
         if title.startswith("M"):
             title = entry.path[10:-4]
             metadata = np.genfromtxt(entry.path,delimiter='=',skip_header=0, skip_footer=514,dtype=str)
-            ra = float(metadata[5][-1])
-            dec = float(metadata[6][-1])
+            dec_degrees, dec_minutes = metadata[6][-1].split(".")
+            dec = float(dec_degrees) + (float(dec_minutes[:2])/60)
+            ra_hours,ra_mins = metadata[5][-1].split(".")
+            ra = (float(ra_hours) + float(ra_mins[:2])/60)*15#np.cos(np.deg2rad(dec))
+            
             file_coordinates[title] = [ra,dec]
-    return file_coordinates
+    return file_coordinates #Returns dictionary
 
 # DEFINE MATHS FUNCTIONS --------------------------------------------------
 
@@ -168,18 +171,35 @@ def remove_local_hydrogen(velocity,flux,title):
 def remove_local_hydrogen_by_average(velocity,flux):
     return velocity, flux- AVG_MILKY_WAY
     #*((AVG_MILKY_WAY/np.max(AVG_MILKY_WAY))*np.max(flux))
-    
+
 #MASS OF HYDROGEN ----------------------------------------------------------
 
 def get_neutral_H_mass(flux,velocity):
-    frequency = velocity*4.762e3
+    frequency = velocity #*4.762e3
     integral = (frequency[1]-frequency[0])*np.sum(flux)
-    integral_s = simpson(flux,frequency)
-    mass_atoms_cm = 3.488e14*integral
-    mass_sol_cm = 8.396e-58*mass_atoms_cm
-    mass_sol_kpc = mass_sol_cm*1/(1.05e-43)
-    mass_sol = mass_sol_kpc*4.83
+    # integral_s = simpson(flux,frequency)
+    # mass_atoms_cm = 3.488e14*integral
+    # mass_sol_cm = 8.396e-58*mass_atoms_cm
+    # mass_sol_kpc = mass_sol_cm*1/(1.05e-43)
+    # mass_sol = mass_sol_kpc*4.83
+    #print(4.762e3*3.488e14*8.396e-58*(1/1.05e-43)) #Should be 14604
+    mass_sol = 14604*integral*4.83
+
+    obs_radius = ((11.6/60)/2 )**2*np.pi #Might needs square angle stuff
+    scale_rectangle = 0.5*0.33
+    mass_sol = (mass_sol/obs_radius)*scale_rectangle
+    print(obs_radius/scale_rectangle)
     return mass_sol
+
+def get_M31_area(ra_coords,dec_coords):
+    x = np.unique(ra_coords)
+    y = np.unique(dec_coords)
+    ra_diff = np.average(np.diff(x))
+    dec_diff = np.average(np.diff(y))
+    ind_area = ra_diff*dec_diff
+    #print(ra_diff,dec_diff)
+    area = ind_area*len(ra_coords)
+    return area
 
 #PLOTTING -------------------------------------------------------------------
 
@@ -234,19 +254,59 @@ def plot_coordinates():
     ra_coords = []
     dec_coords = []
     titles = []
+    fig, ax = plt.subplots()
     for entry in os.scandir("M31Backup\\"):
         title = entry.path[10:-4]
         if title.startswith("M"):
             ra_coords.append(FILE_COORDS[title][0])
             dec_coords.append(FILE_COORDS[title][1])
+            titles.append(title[4:])
 
     plt.xlabel('Right Ascension [degrees]')
     plt.ylabel('Declination [degrees]')
     plt.title("Coordinates")
-    plt.plot(ra_coords,dec_coords,"b.")
+    ax.plot(ra_coords,dec_coords,"r.")
     plt.gca().invert_xaxis()
+    #beam width is 11.6 arcsec
+    ax.set_aspect('equal', adjustable='datalim')
+    radius = (11.6/60)/2
     for i,text in enumerate(titles):
-        plt.annotate(text,(ra_coords[i],dec_coords[i]))
+        circle1 = plt.Circle((ra_coords[i],dec_coords[i]), radius, color='b', fill=False)
+        ax.add_patch(circle1)
+        #ax.annotate(text,(ra_coords[i],dec_coords[i]))
+        height = 0.5#*np.cos(np.deg2rad(dec_coords[i]))
+        width = 0.33
+        rectangle1 = plt.Rectangle((ra_coords[i]-height/2,dec_coords[i]-width/2),height,width,color='g', fill=False)
+        ax.add_patch(rectangle1)
+    print("Observed area is {0:3.2f} square degrees".format(np.pi*radius**2*len(titles)))
+    print("M31 area is {0:3.2f} square degrees".format(get_M31_area(ra_coords, dec_coords)))
+    plt.show()
+
+def plot_contour(ra_coords,dec_coords,results,titles,graph_title):
+    plt.ylabel('Declination [degrees]')
+    plt.xlabel('Right Ascension [degrees]')
+    plt.title(graph_title+" integration contour")
+    plt.gca().invert_xaxis()
+
+    z = np.zeros((9,12))
+    for i,title in enumerate(titles):
+        title_nums = title[4:]
+        x_index = int(title_nums[-1])
+        y_index = int(title_nums[:-1])-1
+        z[x_index][y_index] =  results[i]
+
+    z = np.transpose(z)
+    x = np.unique(ra_coords)
+    y = np.unique(dec_coords)
+    x = np.flip(x)
+    levels=np.linspace(min(results),max(results),15)
+    X,Y = np.meshgrid(x,y) # 12 x 9
+    #plt.xlim(max(x),8.4)
+    #plt.ylim(38.5,42.7)
+    plt.contourf(X,Y,z,levels=levels)
+    #plt.contourf(X,Y,z)
+    #plt.clabel(plt.contour(X,Y,z),inline=True, fontsize=10)
+    plt.colorbar()
     plt.show()
 
 #MAIN FUNCTIONS ------------------------------------------------------------
@@ -279,10 +339,10 @@ def calibrate(path,rtn=False,plot=False):
 
     if plot:
         #ORIGINAL
-        #plot_velocity_baseline(velocity,uncalibrated_flux,title)
+        plot_velocity_baseline(velocity,uncalibrated_flux,title)
 
         #REMOVE BASELINE
-        plot_velocity_without_baseline(velocity,flux_without_baseline,title)
+        #plot_velocity_without_baseline(velocity,flux_without_baseline,title)
 
         #CALIBRATE FOR ELEVATION
         #plot_velocity_without_baseline(velocity,elevation_calibrated_flux,title)
@@ -294,29 +354,29 @@ def calibrate(path,rtn=False,plot=False):
     if rtn:
         return velocity,calibrated_flux
 
-
 def integrate(velocity,flux,title,rtn=False,plot=False):
     #Remove mystery peak
     flux[338:342] = (flux[342] - flux[338]) * np.array([0,1,2,3]) / (velocity[342]-velocity[338]) + flux[338]
-    if int_params[title][2] == "1":
-        min_bound = int(int_params[title][0])
-        max_bound = int(int_params[title][1])
-        if plot:
-            plt.plot([velocity[min_bound],velocity[min_bound]],[0,max(flux)],"r--")
-            plt.plot([velocity[max_bound],velocity[max_bound]],[0,max(flux)],"r--")
-        local_corrected_flux = flux[min_bound:max_bound]
-        local_corrected_velocity = velocity[min_bound:max_bound]
-    if int_params[title][2] == "0":
-        #local_corrected_velocity, local_corrected_flux = remove_local_hydrogen(velocity, flux, title)
-        local_corrected_velocity, local_corrected_flux = remove_local_hydrogen_by_average(velocity, flux)
+
+    min_bound = int(int_params[title][0])
+    max_bound = int(int_params[title][1])
+
+    #local_corrected_velocity, local_corrected_flux = remove_local_hydrogen(velocity, flux, title)
+    local_corrected_velocity, local_corrected_flux = remove_local_hydrogen_by_average(velocity, flux)
+
+    local_corrected_flux = local_corrected_flux[min_bound:max_bound]
+    local_corrected_velocity = local_corrected_velocity[min_bound:max_bound]
+
     if plot:
         plt.plot(velocity, flux,PD,zorder=0)
+        plt.plot([velocity[min_bound],velocity[min_bound]],[0,max(flux)],"r--")
+        plt.plot([velocity[max_bound],velocity[max_bound]],[0,max(flux)],"r--")
         plt.xlabel('Velocity [kms^-1]')
         plt.ylabel('Flux [K]')
         plt.title(title+ ' Velocity-Flux Plot (Corrected)')
-        #plt.xlim([-150,100])
         plt.plot(local_corrected_velocity, local_corrected_flux,"r--",zorder=0)
-        plt.show() #Displays graph
+        plt.show()
+
     area = simpson(local_corrected_flux, local_corrected_velocity)
     mass = get_neutral_H_mass(local_corrected_flux, local_corrected_velocity)
     if rtn:
@@ -330,80 +390,58 @@ def plot_all_graphs():
         if title.startswith("M"):
             calibrate(entry.path,plot=True)
 
-def integrate_all_graphs():
-    results = []
+def integrate_all_graphs(plot=False):
     ra_coords = []
     dec_coords = []
+    areas = []
     titles = []
+    masses = []
     total_mass = 0
     for entry in os.scandir("M31Backup\\"):
         title = entry.path[10:-4]
         if title.startswith("M"):
             titles.append(title)
-            velocity,flux = calibrate(entry.path,rtn=True)
-            area,mass = integrate(velocity,flux,title,rtn=True)
+            velocity, flux = calibrate(entry.path,rtn=True)
+            area, mass = integrate(velocity,flux,title,rtn=True)
             total_mass += mass
-            #if mass<0:
-            #   print(title, mass)
-            print(title, mass)
-            results.append(area)
+            # if mass<0:
+            #    print(title, mass)
+            areas.append(area)
+            masses.append(mass)
             ra_coords.append(FILE_COORDS[title][0])
             dec_coords.append(FILE_COORDS[title][1])
     print("The total mass is {0:3.2e} solar masses".format(total_mass))
     #print(results)
-    plot = False
-    plt.clf()
+
     if plot:
-        
+
         # plt.xlabel('Right Ascension [degrees]')
         # plt.ylabel('Flux [K km^s]')
         # plt.title("Integration by coordinates")
         # plt.plot(ra_coords,results,"rx")
         # plt.gca().invert_xaxis()
         # plt.show()
-    
+
         # plt.xlabel('Declination [degrees]')
         # plt.ylabel('Flux [K km^s]')
         # plt.title("Integration by coordinates")
         # plt.plot(dec_coords,results,"gx")
         # plt.gca().invert_xaxis()
         # plt.show()
-    
-        plt.ylabel('Declination [degrees]')
-        plt.xlabel('Right Ascension [degrees]')
-        plt.title("Flux Integration contour")
-        plt.gca().invert_xaxis()
-    
-        z = np.zeros((9,12))
-        for i,title in enumerate(titles):
-            title_nums = title[4:]
-            x_index = int(title_nums[-1])
-            y_index = int(title_nums[:-1])-1
-            z[x_index][y_index] =  results[i]
-    
-        z = np.transpose(z)
-        x = np.unique(ra_coords)
-        y = np.unique(dec_coords)
-        x = np.flip(x)
-        levels=np.linspace(-150,1100,15)
-        X,Y = np.meshgrid(x,y) # 12 x 9
-        # plt.xlim(0.49,0.31)
-        # plt.ylim(38.5,42.7)
-        plt.contour(X,Y,z,levels=levels)
-        #plt.clabel(plt.contour(X,Y,z),inline=True, fontsize=10)
-        plt.colorbar()
-        plt.show()
+
+        #plot_contour(ra_coords,dec_coords,areas,titles,"Flux")
+        plot_contour(ra_coords,dec_coords,masses,titles,"Neutral hydrogen mass")
 
 
-#plot_coordinates()
 
 FLUX_CALIBRATION = calibrate_flux("M31Backup\\S8A.TXT")
 FILE_COORDS = match_coords()
 AVG_MILKY_WAY = get_average_milkyway()
 #print(FLUX_CALIBRATION)
-
-# individual_path = "M31Backup\\M31P18.TXT"
-# velocity,flux = analyse(individual_path,True)
+plot_coordinates()
+#title = "M31P86"
+#velocity,flux = calibrate("M31Backup\\"+title+".TXT",rtn=True,plot=True)
+#area, mass = integrate(velocity,flux,title,rtn=True,plot=True)
 # plot_velocity_for_integration(velocity,flux,individual_path[10:-4])
 #plot_all_graphs()
-integrate_all_graphs()
+integrate_all_graphs(True)
